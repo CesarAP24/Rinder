2dc2be26bab9e324e61e087707253a379200c491
Gianpier Segovia <gianpier@MacBook-Pro-de-Gianpier.local>
2023-05-02 22:02:06 -0500

    env created, first migrations

--- app.py
+++ app.py
@@ -1,1 +1,17 @@
+#imports
+import psycopg2 
+from flask import(
+    Flask,
+    render_template,
+    jsonify,
+    request,
+)
 
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+
+#configuration
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgresql://postgres:Goleador0107@localhost:5432/rinder'
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
