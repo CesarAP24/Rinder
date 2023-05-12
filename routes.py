fb8a29bf121f151d017e286a71b1007ce476891e
giansegg <gianpier.segovia@utec.edu.pe>
2023-05-12 18:09:28 -0500

    Route /mensajes/list

--- routes.py
+++ routes.py
@@ -150,6 +150,21 @@ def register():
         )
 
     print(perfil)
+    
+@app.route('/mensajes/list', methods=['GET'])
+def mensajes():
+    id_usuario = request.get['id_usuario']
+    mensajes = db.session.query(Mensaje.id_usuarioremitente, Mensaje.id_usuariodestinatario, Mensaje.id_mensaje) \
+        .filter((Mensaje.id_usuarioremitente == id_usuario) | (Mensaje.id_usuariodestinatario == id_usuario)) \
+        .group_by(Mensaje.id_usuarioremitente, Mensaje.id_usuariodestinatario) \
+        .order_by(db.func.MAX(Mensaje.fecha).desc()) \
+        .all()
+
+    resultado = []
+
+
+
+
 
     # gurdar usuario y perfil
 
