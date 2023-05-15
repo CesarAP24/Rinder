38eed6fcd9faa3e03e784e415e09af7dafce99d1
giansegg <gianpier.segovia@utec.edu.pe>
2023-05-14 23:37:16 -0500

    Tokenizamos la data

--- modeloaprendizaje/modelado.py
+++ modeloaprendizaje/modelado.py
@@ -4,48 +4,61 @@ import pandas as pd
 
 data = pd.read_csv('rinderma.csv', delimiter=',')
 
-citaideal = ['Salir a Comer', 'Ir al cine', 'Aire libre', 'Viaje', 'Evento artístico']
-for cita in citaideal:
-    data['Cuál seria tu idea de cita ideal?']=data['Cuál seria tu idea de cita ideal?'].str.split().apply(lambda x: 1 if cita in x else 0)
-    data['Cuál seria tu idea de cita ideal?']=data['Cuál seria tu idea de cita ideal?'].astype(int)
+# columna de cita ideal
+# ----------------------------------------------------------------------
+# creo columnas para cada opción
+for option in ['Salir a Comer', 'Ir al cine', 'Aire libre', 'Viaje', 'Evento artístico']:
+    data[option] = data['¿Cuál seria tu idea de cita ideal?'].str.contains(option).astype('int8')
 
+data.drop('¿Cuál seria tu idea de cita ideal?', axis=1, inplace=True)
+
+
+
+
+# columna edad, estatura, sexo
+# ----------------------------------------------------------------------
 data['¿Cuál es tu edad?']= data['¿Cuál es tu edad?'].astype(int)
 data['¿Cuánto mides? (cm)']= data['¿Cuánto mides? (cm)'].astype(int)
-data['¿Cuál es tu sexo?']= data['Sexo'].map({'Masculino': 0, 'Femenino': 1})
-
-listageneros=['Clásica', 'Rock', 'Pop', 'Reguetón', 'Electrónica', 'otro']
-for genero in listageneros:
-    data['Genero de musica preferido']=data['Genero de musica preferido'].str.split().apply(lambda x: 1 if genero in x else 0)
-    data['Genero de musica preferido']=data['Genero de musica preferido'].astype(int)
+data['¿Cuál es tu sexo?']= data['¿Cuál es tu sexo?'].map({'Masculino': 0, 'Femenino': 1})
 
 
+# columna de musica
+# ----------------------------------------------------------------------
+# crear columna para cada opción
+for option in ['Clásica', 'Rock', 'Pop', 'Reguetón', 'Electrónica', 'otro']:
+    data[option] = data['Género de musica preferido '].str.contains(option).astype('int8')
 
-listapeliculas=['Drama', 'Thriller / suspense/', 'Comedia', 'Acción', 'Aventuras', 'Otro']
-for pelicula in listapeliculas:
-    data['Género de pelicula preferido']=data['Género de pelicula preferido'].str.split().apply(lambda x: 1 if pelicula in x else 0)
-    data['Género de pelicula preferido']=data['Género de pelicula preferido'].astype(int)
+data.drop('Género de musica preferido ', axis=1, inplace=True)
 
 
-deportespreferidos= ['Fútbol', 'Basket', 'Tenis', 'Natación', 'Atletismo', 'Otro']
-for deporte in deportespreferidos:
-    data['Deportes preferidos']=data['Deportes preferidos'].str.split().apply(lambda x: 1 if deporte in x else 0)
-    data['Deportes preferidos']=data['Deportes preferidos'].astype(int)
 
-data['¿Vas al gimnasio?']=data['¿Vas al gimnasio?'].map({'No':0, 'Si':1})
+# columna de peliculas
+# ----------------------------------------------------------------------
+for option in ['Drama', 'Thriller / suspense/', 'Comedia', 'Acción', 'Aventuras', 'Otro']:
+    data[option] = data['Género de pelicula preferido'].str.contains(option).astype('int8')
+
+data.drop('Género de pelicula preferido', axis=1, inplace=True)
+
+
+# columna de deportes
+# ----------------------------------------------------------------------
+for option in ['Fútbol', 'Basket', 'Tenis', 'Natación', 'Atletismo', 'Otro']:
+    data[option] = data['Deportes preferidos '].str.contains(option).astype('int8')
+
+data.drop('Deportes preferidos ', axis=1, inplace=True)
+
+
+# columna gimnasio, salir, beber
+# ----------------------------------------------------------------------
+data['¿Vas al gimnasio? ']=data['¿Vas al gimnasio? '].map({'No':0, 'Si':1})
 data['¿Te gusta salir mucho?']=data['¿Te gusta salir mucho?'].astype(int)
-data['¿Te gusta beber?']=data['¿Te gusta beber?'].map.as_type(int)
-
-oficios= ['Estudias', 'Trabajas', 'Ambos', 'Ninguno']
-for oficio in oficios:
-    data['¿A qué te dedicas?']=data['¿A qué te dedicas?'].str.split().apply(lambda x: 1 if oficio in x else 0)
-    data['¿A qué te dedicas?']=data['¿A qué te dedicas?'].astype(int)
+data['¿Te gusta beber?']=data['¿Te gusta beber?'].astype(int)
+data['¿A qué te dedicas?']=data['¿A qué te dedicas?'].map({'Estudias':0, 'Trabajas':1, 'Ambos':2, 'Ninguno':3}).astype(int)
+data['Orientación Sexual']=data['Orientación Sexual'].map({'Heterosexual':0, 'Bisexual':1, 'Gay':2, 'otra':3}).astype(int)
 
 
-horientaciones= ['Heterosexual', 'Bisexual', 'Gay', 'otra']
-for horientacion in horientaciones:
-    data['Orientación Sexual']=data['Orientación Sexual'].str.split().apply(lambda x: 1 if horientacion in x else 0)
-    data['Orientación Sexual']=data['Orientación Sexual'].astype(int)
+#imprimir el dato 11
+print(data.iloc[11])
 
-
-hola=data['Deportes preferidos']
-
+#imprimir las columnas
+print(data.columns)
