de0283db706f2944343425afe59139e8807da222
giansegg <gianpier.segovia@utec.edu.pe>
2023-07-11 00:34:03 -0500

    Usuario y suscripcion

--- newRinder/backend/app/__init__.py
+++ newRinder/backend/app/__init__.py
@@ -4,7 +4,7 @@ from .models import db, Usuario, Perfil, Chat, Mensaje, Suscripcion, Compra, Lik
 from flask_cors import CORS
 from .utilities import *
 
-from flask import(
+from flask import (
     Flask,
     render_template,
     jsonify,
@@ -17,7 +17,9 @@ from flask_jwt_extended import JWTManager, jwt_required, create_access_token
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
 from sqlalchemy import ForeignKey
+from flask_bcrypt import Bcrypt
 
+import sys
 import uuid
 import json
 from datetime import datetime
@@ -28,11 +30,11 @@ from flask_bcrypt import Bcrypt
 
 def create_app(test_config=None):
     app = Flask(__name__)
-    app.secret_key = 'pneumonoultramicroscopicsilicovolcanoconiosis';
+    app.secret_key = 'pneumonoultramicroscopicsilicovolcanoconiosis'
     app.config['JWT_SECRET_KEY'] = 'pneumonoultramicroscopicsilicovolcanoconiosis'
-    bcrypt = Bcrypt(app);
-    jwt = JWTManager(app);
-    
+    bcrypt = Bcrypt(app)
+    jwt = JWTManager(app)
+
     with app.app_context():
         setup_db(app, test_config['database_path'] if test_config else None)
         CORS(app, origins='*')
@@ -40,24 +42,10 @@ def create_app(test_config=None):
     @app.after_request
     def after_request(response):
         response.headers.add('Access-Control-Allow-Headers', 'Content-Type')
-        response.headers.add('Access-Control-Allow-Methods', 'GET,PATCH,POST,DELETE,OPTIONS')
+        response.headers.add('Access-Control-Allow-Methods',
+                             'GET,PATCH,POST,DELETE,OPTIONS')
         response.headers.add(' Access-Control-Max-Age', '10')
         return response
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     # Routes API
 
@@ -68,7 +56,7 @@ def create_app(test_config=None):
         chat = Chat.query.filter_by(id_chat=id).first()
         user_id = get_jwt_identity()
 
-        #checkear si el usuario participa en el chat
+        # checkear si el usuario participa en el chat
         chats = Pertenece.query.filter_by(id_usuario=user_id).all()
         id_chats = [chat.id_chat for chat in chats]
 
@@ -83,8 +71,6 @@ def create_app(test_config=None):
                 'success': False,
                 'error': 'El usuario no participa en el chat'
             }), 401
-
-
 
     @app.route('/chats', methods=['GET'])
     @jwt_required
@@ -105,7 +91,6 @@ def create_app(test_config=None):
                 'error': 'El usuario no tiene chats'
             }), 404
 
-
     @app.route('/perfiles', methods=['GET'])
     def get_perfil():
         perfiles = Perfil.query.all()
@@ -113,8 +98,6 @@ def create_app(test_config=None):
             'success': True,
             'perfiles': [perfil.serialize() for perfil in perfiles]
         }), 200
-
-    
 
     @app.route('/suscripciones', methods=['GET'])
     def get_suscriptions():
@@ -129,7 +112,6 @@ def create_app(test_config=None):
             'success': True,
             'suscripciones': [suscripcion.serialize() for suscripcion in suscripciones]
         }), 200
-
 
     @app.route('/compras', methods=['GET'])
     def get_compras():
@@ -149,50 +131,156 @@ def create_app(test_config=None):
     def patch_compras():
         abort(501)
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
     # POST -----------------------------------------------------------------
 
     @app.route('/usuarios', methods=['POST'])
     def post_users():
-        abort(501)
+        returned_code = 201
+        list_errors = []
+        try:
+            body = request.json
+            if 'correo' not in body:
+                list_errors.append('correo is required')
+            else:
+                correo = body['correo']
+                if Usuario.query.filter_by(correo=correo).first():
+                    list_errors.append('correo already exists')
+            if 'contraseña' not in body:
+                list_errors.append('contraseña is required')
+            else:
+                hashed_password = bcrypt.generate_password_hash(
+                    body['contraseña']).decode('utf-8')
+                contraseña = hashed_password
 
-    @app.route('/mensajes', methods=['POST'])
-    def post_mensajes():
-        abort(501)
+            if len(list_errors) > 0:
+                returned_code = 400
+            else:
+                id_usuario = str(uuid.uuid4())
+                usuario = Usuario(id_usuario=id_usuario,
+                                  correo=correo, contraseña=contraseña)
+                db.session.add(usuario)
+                db.session.commit()
 
+                return jsonify({
+                    "success": True,
+                    "message": "Usuario creado exitosamente",
+                    "usuario": usuario.serialize()
+                }), returned_code
+        except Exception as e:
+            print(sys.exc_info())
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+        if returned_code == 400:
+            return jsonify({"success": False, "message": 'Error creating User', 'errors': list_errors}), returned_code
+        else:
+            return jsonify({"success": True, 'message': 'User created successfully'}), returned_code
+
+    @app.route('/chats/<id>/mensajes', methods=['POST'])
+    @jwt_required
+    def post_mensajes(id):
+        id_usuario = get_jwt_identity()
+        id_chat = id
+        id_mensajePadre = None
+        returned_code = 201
+        list_errors = []
+        try:
+            body = request.json
+            currently_chat = Chat.query.filter_by(id_chat=id_chat).first()
+            id_last_mensaje = currently_chat.id_mensaje
+            if not currently_chat:
+                list_errors.append('Chat does not exist')
+            if 'contenido' not in body:
+                list_errors.append('contenido is required')
+            else:
+                contenido = body['contenido']
+            if len(list_errors) > 0:
+                returned_code = 400
+
+            else:
+                id_mensaje = str(uuid.uuid4())
+                mensaje = Mensaje(id_mensaje=id_mensaje, id_usuario=id_usuario,
+                                  id_chat=id_chat, id_mensajePadre=id_last_mensaje, contenido=contenido)
+                db.session.add(mensaje)
+                db.session.commit()
+                currently_chat.id_mensaje = id_mensaje
+                db.session.commit()
+
+                return jsonify({
+                    "success": True,
+                    "message": "Mensaje creado exitosamente",
+                    "mensaje": mensaje.serialize()
+                }), returned_code
+        except Exception as e:
+            print(sys.exc_info())
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+        if returned_code == 400:
+            return jsonify({"success": False, "message": 'Error creating Mensaje', 'errors': list_errors}), returned_code
+        else:
+            return jsonify({"success": True, 'message': 'Mensaje created successfully'}), returned_code
+        
+
+    class Suscripcion(db.Model):
+    __tablename__ = 'suscripcion'
+    nombre = db.Column(db.String(50), primary_key=True)
+    precio = db.Column(db.Float, nullable = False)
+    day_duration = db.Column(db.Integer, default=30)
+
+    created = db.Column(db.DateTime, default=datetime.utcnow())
+    modified = db.Column(db.DateTime, default=datetime.utcnow(), onupdate=datetime.utcnow()) 
 
     @app.route('/suscripciones', methods=['POST'])
+    @jwt_required
     def post_suscriptions():
-        abort(501)
+        creadores = ['casurpiemelisante', 'giansegg', 'isabellaromero']
+        id_usuario = get_jwt_identity()
+        returned_code = 201
+        list_errors = []
+        try:
+            body = request.json
+            
+            if id_usuario in creadores:  
+                if 'nombre' not in body:
+                    list_errors.append('nombre is required')
+                else:
+                    nombre = body['nombre']
+                
+                if 'precio' not in body:
+                    list_errors.append('precio is required')
+                else:
+                    precio = body['precio']
+
+                if len(list_errors) > 0:
+                    returned_code = 400
+                else:
+                    suscripcion = Suscripcion(nombre=nombre, precio=precio)
+                    db.session.add(suscripcion)
+                    db.session.commit()
+
+                    return jsonify({
+                        "success": True,
+                        "message": "Suscripcion creada exitosamente",
+                        "suscripcion": suscripcion.serialize()
+                    }), returned_code
+            else:
+                return jsonify({"success": False, "message": 'No tienes permiso para crear suscripciones'}), 401
+            
+        except Exception as e:
+            print(sys.exc_info())
+            db.session.rollback()
+            returned_code = 500
+        finally:
+            db.session.close()
+        
+        if returned_code == 400:
+            return jsonify({"success": False, "message": 'Error creating Suscripcion', 'errors': list_errors}), returned_code
+        else:
+            return jsonify({"success": True, 'message': 'Suscripcion created successfully'}), returned_code
+
 
     @app.route('/chats', methods=['POST'])
     def post_compras():
@@ -206,8 +294,6 @@ def create_app(test_config=None):
     def post_likes():
         abort(501)
 
-    
-
     # DELETE ---------------------------------------------------------------
 
     @app.route('/usuarios', methods=['DELETE'])
@@ -218,60 +304,40 @@ def create_app(test_config=None):
     def delete_suscriptions():
         abort(501)
 
-
-
     # LOGIN ----------------------------------------------------------------
 
     @app.route('/api/login', methods=['POST'])
     def login():
         abort(501)
-    
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 
     # HANDLE ERROR ---------------------------------------------------------
 
     @app.errorhandler(404)
     def not_found(error):
-        return jsonify({ "success": False, "message": 'Resource not found' }), 404
+        return jsonify({"success": False, "message": 'Resource not found'}), 404
 
     @app.errorhandler(401)
     def unauthorized(error):
-        return jsonify({ "success": False, "message": 'Unauthorized' }), 401
+        return jsonify({"success": False, "message": 'Unauthorized'}), 401
 
     @app.errorhandler(400)
     def bad_request(error):
-        return jsonify({ "success": False, "message": 'Bad request' }), 400
+        return jsonify({"success": False, "message": 'Bad request'}), 400
 
     @app.errorhandler(405)
     def not_allowed(error):
-        return jsonify({ "success": False, "message": 'Method not allowed' }), 405
+        return jsonify({"success": False, "message": 'Method not allowed'}), 405
 
     @app.errorhandler(403)
     def forbidden(error):
-        return jsonify({ "success": False, "message": 'Forbidden' }), 403
-
+        return jsonify({"success": False, "message": 'Forbidden'}), 403
 
     @app.errorhandler(500)
     def server_error(error):
-        return jsonify({ "success": False, "message": 'Server error' }), 500
+        return jsonify({"success": False, "message": 'Server error'}), 500
 
     @app.errorhandler(501)
     def not_implemented(error):
-        return jsonify({ "success": False, "message": 'Not implemented' }), 501
+        return jsonify({"success": False, "message": 'Not implemented'}), 501
 
     return app
-
